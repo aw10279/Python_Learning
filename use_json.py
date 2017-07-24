c30efd54f64ee516ba9d602ee082174a69da7fe4
aw10279 <aw10279@163.com>
2017-07-24 17:07:39 +0800

    Using json for Serialization

---
+++ use_json.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+class Std(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+def s2d(s):
+    return {'name':s.name, 'age':s.age, 'score':s.score}#实例转化为dict
+
+def d2s(d):
+    return Std(d['name'], d['age'], d['score'])#dict转化为实例
+
+
+s = Std('Bob', 20, 88)
+print(json.dumps(s, default = s2d))
+#print(json.dumps(s, default = lambda obj: obj.__dict__))  default后面可用通用格式
+
+
+d = '{"age": 20, "score": 88, "name": "Bob"}'   #json必须以字符串为参数
+t = json.loads(d, object_hook=d2s)              #转化为实例对象
+
+print(t.name, t.age, t.score)
