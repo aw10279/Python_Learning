d6b37afc81bbf0a3833539cee317b0a7852856ca
aw10279 <aw10279@163.com>
2017-07-28 13:35:56 +0800

    Solving the quadratic with math

---
+++ quadratic.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+def quadratic(a,b,c):
+
+    dt = b*b-4*a*c
+    if dt < 0:
+        print('该组参数无解')
+    elif dt == 0:
+        print('x1 = x2 =',-b/(2*a))
+    else:
+        x1 = (-b+math.sqrt(dt))/(2*a)
+        x2 = (-b-math.sqrt(dt))/(2*a)
+        print('x1 =',x1,'\nx2 =',x2)
+    return
