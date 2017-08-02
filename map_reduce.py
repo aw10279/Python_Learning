a2c77137410f479cb3df5a7e81358492322121fe
aw10279 <aw10279@163.com>
2017-08-02 09:50:21 +0800

    realizing str 2 float with map&reduce

---
+++ map_reduce.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from functools import reduce
+
+def str2float(s):
+
+    index = s.find('.')         #用string的find和replace属性确定除以的倍数，并去掉小数点。
+    time = len(s)-index-1
+    L = s.replace(".","")
+
+    def fn(x, y):               #reduce该函数，合并成整数
+        return x * 10 + y
+
+    def char2num(s):            #map该函数，完成str-num的映射
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+        
+    num = reduce(fn, map(char2num, L))
+    return num/(10**time)       #得到浮点数
+
+print(str2float('1234.56'))
