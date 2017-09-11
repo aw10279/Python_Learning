334dc1d26f1c41090919558ceab143f33a5f3445
aw10279 <aw10279@163.com>
2017-09-11 14:15:58 +0800

    distributed process done

--- distributed_client.py
+++ distributed_client.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
+#------------------------------------------------client进程，用于执行任务并返回结果---------------------------
+
 import queue, time, math
 from multiprocessing.managers import BaseManager
 
 class QueueManager(BaseManager):
     pass
 
-QueueManager.register('get_task_queue')
+QueueManager.register('get_task_queue')                #2个队列都直接从网络获取，因此本身无需定义，直接注册接口即可
 QueueManager.register('get_result_queue')
 
 server_adr = '127.0.0.1'
@@ -17,14 +19,14 @@ m.connect()
 task = m.get_task_queue()
 result = m.get_result_queue()
 
-for i in range(10):
+for i in range(10):                                   #执行task中的任务，并将结果放入result
 	try:
 		n = task.get(timeout=1)
 		print('run task %d...' % n)
 		r = 'the square root of %d = %f' % (n, math.sqrt(n))
 		time.sleep(1)
 		result.put(r)
-	except Queue.Empty:
+	except Queue.Empty:                              #遇到empty异常跳出循环
 		print('task queue is empty.')
 
 print('Task exit.')
--- distributed_server.py
+++ distributed_server.py
@@ -1,39 +1,41 @@
 # -*- coding: utf-8 -*-
 
+#-----------------------------------------------server进程，用于分发任务并接收结果----------------------------------
+
 import queue, random, time
 from multiprocessing.managers import BaseManager
 from multiprocessing import freeze_support
 
-task_queue = queue.Queue()
+task_queue = queue.Queue()                      #新建任务队列和结果队列
 result_queue = queue.Queue()
 
-class QueueManager(BaseManager):
+class QueueManager(BaseManager):               #让QueueManager继承所有Basemanager的方法
     pass
 
-def t_queue():
+def t_queue():                                 #建2个返回队列的函数，专用于下面的网络调用
     return task_queue
 def r_queue():
     return result_queue
 
-QueueManager.register('get_task_queue', callable=t_queue)
+QueueManager.register('get_task_queue', callable=t_queue)     #将2个队列在网络上注册，并定义网络接口名称
 QueueManager.register('get_result_queue', callable=r_queue)
 
-manager = QueueManager(address=('127.0.0.1', 5000), authkey=b'abc')
+manager = QueueManager(address=('127.0.0.1', 5000), authkey=b'abc')   #实例化QueueManager并定义IP、端口号、验证码
 
-def server_start():
+def server_start():                           #将manager的启动封装到一个函数内，用于下面的main函数调用
     manager.start()
 
-    task = manager.get_task_queue()
+    task = manager.get_task_queue()           #从网络渠道获取2个队列（非本地获取）
     result = manager.get_result_queue()
 
-    for i in range(10):
+    for i in range(10):                       #将任务放入task队列
         n = random.randint(0, 10000)
         print('send task %d...' % n)
         task.put(n)
 
     print('Try get results...')
 
-    for i in range(10):
+    for i in range(10):                       #从result队列获取结果
         r = result.get(timeout=10)
         print('%s' % r)
 
